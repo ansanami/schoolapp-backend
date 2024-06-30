fe786fab7b828bf885512d962f6004985c7a1ed0
Emir Tarlabelen <emirtarlabelen021@gmail.com>
2024-06-30 20:51:34 +0300

    seats func created

--- src/main/java/tr/ogu/edu/school/schoolapp/mapper/ActSessionHallMapper.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/mapper/ActSessionHallMapper.java
@@ -6,30 +6,39 @@ import tr.ogu.edu.school.schoolapp.model.ActSessionHallSeat;
 
 public class ActSessionHallMapper extends DtoEntityMapper<ActSessionHallSeat, ActSessionHallSeatDto> {
 
-	public static final ActSessionHallMapper INSTANCE = new ActSessionHallMapper();
 
-	@Override
-	public ActSessionHallSeatDto toDto(ActSessionHallSeat entity) {
-		ActSessionHallSeatDto dto = new ActSessionHallSeatDto();
-		dto.setId(entity.getId());
-		dto.setBlockedTime(entity.getBlockedTime());
-		dto.setStatus(entity.getStatus());
-		dto.setActSeat(ActSeatMapper.INSTANCE.toDto(entity.getActSeat()));
-		UserDto userDto = UserMapper.toUserDto(entity.getUser());
-		dto.setUser(userDto);
-		dto.setBlockedTime(entity.getBlockedTime());
-		dto.setActSessionInfo(ActSessionInfoMapper.INSTANCE.toDto(entity.getActSessionInfo()));
-		return dto;
-	}
+    public static final ActSessionHallMapper INSTANCE = new ActSessionHallMapper();
 
-	@Override
-	public ActSessionHallSeat toEntity(ActSessionHallSeatDto dto) {
-		ActSessionHallSeat entity = new ActSessionHallSeat();
-		entity.setId(dto.getId());
-		entity.setBlockedTime(dto.getBlockedTime());
-		entity.setStatus(dto.getStatus());
-		entity.setActSeat(ActSeatMapper.INSTANCE.toEntity(dto.getActSeat()));
-		return entity;
-	}
+    @Override
+    public ActSessionHallSeatDto toDto(ActSessionHallSeat entity) {
+        ActSessionHallSeatDto dto = new ActSessionHallSeatDto();
+        dto.setId(entity.getId());
+        dto.setBlockedTime(entity.getBlockedTime());
+        dto.setStatus(entity.getStatus());
+        dto.setActSeat(ActSeatMapper.INSTANCE.toDto(entity.getActSeat()));
+
+        // Check if user is null before mapping
+        if (entity.getUser() != null) {
+            UserDto userDto = UserMapper.toUserDto(entity.getUser());
+            dto.setUser(userDto);
+        } else {
+            dto.setUser(null); // or handle it as needed
+        }
+
+        dto.setBlockedTime(entity.getBlockedTime());
+        dto.setActSessionInfo(ActSessionInfoMapper.INSTANCE.toDto(entity.getActSessionInfo()));
+        return dto;
+    }
+
+    @Override
+    public ActSessionHallSeat toEntity(ActSessionHallSeatDto dto) {
+        ActSessionHallSeat entity = new ActSessionHallSeat();
+        entity.setId(dto.getId());
+        entity.setBlockedTime(dto.getBlockedTime());
+        entity.setStatus(dto.getStatus());
+        entity.setActSeat(ActSeatMapper.INSTANCE.toEntity(dto.getActSeat()));
+        // Mapping user entity might be required if needed
+        return entity;
+    }
 
 }
